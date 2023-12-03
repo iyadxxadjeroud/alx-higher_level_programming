56b45dc9aedbe0cf5878c1795cbcd9de215bbd94
iyadxxadjeroud <iyadadjeroud10@gmail.com>
2023-12-03 23:39:29 +0100

    DOne

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
     if idx < 0:
         return None
     elif idx >= len(my_list):
         return None
     else:
-        print("Element at index {:d} is {:d}".format(idx, my_list[idx]))
+        my_list[idx] = element
