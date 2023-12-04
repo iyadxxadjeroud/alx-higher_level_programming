a2e007fac98646b8d739e61827d3a42b391be5c8
iyadxxadjeroud <iyadadjeroud10@gmail.com>
2023-12-04 23:36:25 +0100

    Done

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
     ret = ""
-    for i in range(len(my_string)):
-        if (my_string[i] != "c" and my_string != "C"):
-            ret += my_string[i]
+    for elements in my_string:
+        if elements != "c" and elements != "C":
+            ret += elements
     return ret
