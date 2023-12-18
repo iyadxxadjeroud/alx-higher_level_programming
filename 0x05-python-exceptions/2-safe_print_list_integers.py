aad1e611acc4b85349985f102567a8a349603ff6
iyadxxadjeroud <iyadadjeroud10@gmail.com>
2023-12-18 23:20:59 +0100

    DONE

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
+def safe_print_list_integers(my_list=[], x=0):
     i, c = 0, 0
     while i < x:
         try:
-            print('{:d}'.format(my_list[i]), end='')
+            print("{:d}".format(my_list[i]), end='')
             c += 1
         except (ValueError, TypeError):
             pass
